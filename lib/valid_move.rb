11b3241347ddc429691c0451260b34f127e2a503
Aleksandr <alexander.butya@gmail.com>
2020-11-27 15:29:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def position_taken?(board, index)
   end
 end
 def valid_move?(board, index)
-  if position_taken?(board, index) or !between?(0,8)
+  if position_taken?(board, index) or !index.between?(0,8)
     return false
   else
     return true
